dd791679c189ce1c7b6acc81db69eb0cca7255aa
CCCCesar <rogeliogc147@gmail.com>
2019-02-03 19:21:53 -0800

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -17,6 +17,6 @@ let urlDB;
 if(process.env.NODE_ENV === 'dev'){
     urlDB ='mongodb://localhost:27017/coffee'
 }else{
-    urlDB = process.env.NODE_URI;
+    urlDB = process.env.MONGO_URI;
 }
 process.env.URLDB = urlDB;
