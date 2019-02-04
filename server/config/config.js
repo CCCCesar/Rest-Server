77e68597d2c2903f843d24b069145ea006b32dfb
CCCCesar <rogeliogc147@gmail.com>
2019-02-03 17:30:42 -0800

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,4 +1,22 @@
 //=========================================
-//puerto
+//Puerto
 //=========================================
 process.env.PORT = process.env.PORT || 3000;
+
+
+//=========================================
+//Entorno
+//=========================================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+
+//=========================================
+//Base de Datos
+//=========================================
+let urlDB;
+// if(process.env.NODE_ENV === 'dev'){
+    // urlDB ='mongodb://localhost:27017/coffee'
+// }else{
+    urlDB = 'mongodb://coffe-user:gonzas123456789@ds121135.mlab.com:21135/coffee'
+// }
+process.env.URLDB = urlDB;
