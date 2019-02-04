df06cff4a972f370ecc431496480e240740253d9
CCCCesar <rogeliogc147@gmail.com>
2019-02-03 17:55:12 -0800

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -14,9 +14,9 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 //Base de Datos
 //=========================================
 let urlDB;
-// if(process.env.NODE_ENV === 'dev'){
-    // urlDB ='mongodb://localhost:27017/coffee'
-// }else{
+if(process.env.NODE_ENV === 'dev'){
+    urlDB ='mongodb://localhost:27017/coffee'
+}else{
     urlDB = 'mongodb://coffe-user:gonzas123456789@ds121135.mlab.com:21135/coffee'
-// }
+}
 process.env.URLDB = urlDB;
